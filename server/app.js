e342183226129eb960e11fecfe02c3cc42874a0a
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-22 11:18:10 +0330

    complete-front-and-back

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
-import express from 'express';
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { guessNumber } from "./src/services/guess-number.js";
 const app = express();
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-const port = 3000;
+const port = 80;
 app.use(express.json());
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use("/", express.static(join(__dirname, "../client")));
 
-app.use('/',express.static(join(__dirname,'../client')));
-
+app.post("/guessNumber", (req, res) => {
+  let guess = req.body.data;
+  let result = guessNumber(guess);
+  res.status(200).send(result);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
