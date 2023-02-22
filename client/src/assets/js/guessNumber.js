51a0cb78960b657f1e89a3bf65d755e62d3cab06
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-22 11:57:38 +0330

    Feat:Show-result-box-&-guess-number-front-script

---
+++ client/src/assets/js/guessNumber.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("submit");
+const input = document.getElementById("guessinput");
+const ptag1 = document.getElementById("msg1");
+const ptag2 = document.getElementById("msg2");
+const ptag3 = document.getElementById("msg3");
+
+function sendRequest() {
+  const data = { data: input.value };
+  fetch("/guessNumber", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      ptag1.innerHTML = data.msg1;
+      ptag2.innerHTML = data.msg2;
+      ptag3.innerHTML = data.msg3;
+    });
+}
+btn.addEventListener("click", sendRequest);
