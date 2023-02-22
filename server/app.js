f761daa9837b61a61bcf59fff2525842af3b0c08
sajjad ghorbani <sajjadr2001@gmail.com>
2023-02-22 11:02:34 +0330

    feat: create server and  api route for guess number

--- server/app.js
+++ server/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
-const app = express();
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-const port = 3000;
-app.use(express.json());
-
+import { chance, checkanswer } from './src/services/guess-number.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const app = express();
+const port = 80;
 
 
-app.use('/',express.static(join(__dirname,'../client')));
+app.use(express.json());
+app.use('/', express.static(join(__dirname, '../client')));
 
+
+app.post('/guessnumber/checkanswer', (req, res) => {
+
+  let guess = req.body.guess;
+  console.log(guess);
+  let message = checkanswer(guess);
+  res.send(JSON.stringify({ "message": message, "chance": chance }));
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
