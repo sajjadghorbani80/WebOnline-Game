59a4bf221d924839d4cc87b3d085d3ee2f8536bf
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-04-05 12:00:21 +0330

    Start: user register for singup with validations

--- server/routes/authRouters.js
+++ server/routes/authRouters.js
@@ -1,9 +1,22 @@
+/* eslint-disable max-len */
 /* eslint-disable new-cap */
 
 import {Router} from 'express';
 import {ReqDto} from '../src/dtos/userRegisterDto.js';
 import {userRegister} from '../src/services/authServices.js';
 import {generateToken} from '../src/services/authServices.js';
+import {validationResult, check} from 'express-validator';
+import { ResponseDto } from '../src/dtos/responseDto.js';
+
+const usernameRegex= '^[A-Za-z][A-Za-z0-9_]{3,20}$';
+const validationRules = [check('username').trim()
+    .escape().notEmpty().withMessage('username.input.empty')
+    .custom((username) => username.match(usernameRegex) ? true:false).withMessage('username.input.invalid'),
+check('email').trim().escape().notEmpty().withMessage('email.input.empty')
+    .isEmail().withMessage('email.input.invalid'),
+check('fullname').trim().notEmpty().withMessage('fullname.input.empty'),
+check('password').escape().notEmpty().withMessage('password.input.empty')];
+
 
 const router = Router();
 
@@ -17,10 +30,15 @@ router.get('/user/validateToken', (req, res) => {
 
 });
 
-router.post('/user/userregister', async (req, res)=>{
+router.post('/user/userregister', validationRules, async (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const response = new ResponseDto(400, null, errors);
+    return res.status(200).send(response);
+  }
   const result = new ReqDto(req.body.username, req.body.email,
       req.body.fullname, req.body.password);
   const userRegisterResult = await userRegister(result);
-  return userRegisterResult;
+  res.send(userRegisterResult);
 });
 export {router};
